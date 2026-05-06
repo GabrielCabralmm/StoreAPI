118d7230e83db4e4f2ceed7ba7dabb7052460263
GabrielCabralmm <gabriel.cmmariano@gmail.com.br>
2026-05-05 21:20:42 -0300

    Escrevendo os métodos CRUD dentro das controllers.

--- StoreAPI-CP2/Controllers/ClienteController.cs
+++ StoreAPI-CP2/Controllers/ClienteController.cs
@@ -1,12 +1,116 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNET.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using StoreAPI_CP2.Entities;
 
 namespace StoreAPI_CP2.Controllers
 {
+    [Route("api/cliente")]
+    [ApiController]
     public class ClienteController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationContext _context;
+
+        public ClienteController(ApplicationContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetClientes()
+        {
+            try
+            {
+                var resultado = _context.Cliente.ToList();
+
+                if (!resultado.Any())
+                    return NoContent();
+
+                return Ok(resultado);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCliente(int id)
+        {
+            try
+            {
+                var cliente = _context.Cliente.FirstOrDefault(x => x.Id == id);
+
+                if (cliente is null)
+                    return NotFound();
+
+                return Ok(cliente);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddCliente(ClienteEntity model)
+        {
+            try
+            {
+                _context.Cliente.Add(model);
+                _context.SaveChanges();
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IActionResult EditCliente(int id, ClienteEntity model)
+        {
+            try
+            {
+                var cliente = _context.Cliente.FirstOrDefault(x => x.Id == id);
+
+                if (cliente is null)
+                    return NotFound();
+
+                cliente.Nome = model.Nome;
+                cliente.CPF = model.CPF;
+                cliente.Email = model.Email;
+                cliente.Telefone = model.Telefone;
+
+                _context.Cliente.Update(cliente);
+                _context.SaveChanges();
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCliente(int id)
+        {
+            try
+            {
+                var cliente = _context.Cliente.FirstOrDefault(x => x.Id == id);
+
+                if (cliente is null)
+                    return NotFound();
+
+                _context.Cliente.Remove(cliente);
+                _context.SaveChanges();
+
+                return Ok(cliente);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
--- StoreAPI-CP2/Controllers/ProdutoController.cs
+++ StoreAPI-CP2/Controllers/ProdutoController.cs
@@ -1,12 +1,117 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNET.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using StoreAPI_CP2.Entities;
 
 namespace StoreAPI_CP2.Controllers
 {
+    [Route("api/produto")]
+    [ApiController]
     public class ProdutoController : Controller
     {
-        public IActionResult Index()
+
+        private readonly ApplicationContext _context;
+
+        public ProdutoController(ApplicationContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetProdutos()
+        {
+            try
+            {
+                var resultado = _context.Cliente.ToList();
+
+                if (!resultado.Any())
+                    return NoContent();
+
+                return Ok(resultado);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduto(int id)
+        {
+            try
+            {
+                var produto = _context.Produto.FirstOrDefault(x => x.Id == id);
+
+                if (produto is null)
+                    return NotFound();
+
+                return Ok(produto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddProduto(ProdutoEntity model)
+        {
+            try
+            {
+                _context.Produto.Add(model);
+                _context.SaveChanges();
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IActionResult EditProduto(int id, ProdutoEntity model)
+        {
+            try
+            {
+                var produto = _context.Produto.FirstOrDefault(x => x.Id == id);
+
+                if (produto is null)
+                    return NotFound();
+
+                produto.Produto = model.Produto;
+                produto.Categoria = model.Categoria;
+                produto.Preco = model.Preco;
+                produto.QtdEstoque = model.QtdEstoque;
+
+                _context.Produto.Update(produto);
+                _context.SaveChanges();
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduto(int id)
+        {
+            try
+            {
+                var produto = _context.Produto.FirstOrDefault(x => x.Id == id);
+
+                if (produto is null)
+                    return NotFound();
+
+                _context.Produto.Remove(produto);
+                _context.SaveChanges();
+
+                return Ok(produto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
